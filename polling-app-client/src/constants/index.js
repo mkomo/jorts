9e48de0de3e35e6940f246ad390c3a1ea79b6a40
Matt Komorowski <matt@mkomo.com>
2018-08-26 23:33:36 -0400

    working after switch to postgres

--- polling-app-client/src/constants/index.js
+++ polling-app-client/src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:5000/api';
+export const API_BASE_URL = 'http://localhost:8080/jorts/api';
 //export const API_BASE_URL = '/api';
 export const ACCESS_TOKEN = 'accessToken';
 
